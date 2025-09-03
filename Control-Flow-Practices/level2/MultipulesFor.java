d8befb590e2dd54a77269c199f4280e25ee88dd6
Rohit Sharma <rohitsharma981818@gmail.com>
2025-09-03 18:08:43 +0530

    added

---
+++ Control-Flow-Practices/level2/MultipulesFor.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class MultiplesFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer less than 100: ");
+        int number = sc.nextInt();
+
+        if (number < 1 || number >= 100) {
+            System.out.println("Invalid input. Enter a positive integer less than 100.");
+        } else {
+            System.out.println("Multiples of " + number + " below 100 are:");
+            for (int i = 100; i >= 1; i--) {
+                if (i % number == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
