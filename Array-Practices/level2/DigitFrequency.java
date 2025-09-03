2fd0d9362742f7948ba3d52073395a273dde5a26
Rohit Sharma <rohitsharma981818@gmail.com>
2025-09-03 16:46:30 +0530

    added

---
+++ Array-Practices/level2/DigitFrequency.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        long number = sc.nextLong();
+        int[] freq = new int[10];
+
+        while (number != 0) {
+            int digit = (int)(number % 10);
+            freq[digit]++;
+            number /= 10;
+        }
+
+        System.out.println("Digit frequencies:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digit " + i + ": " + freq[i]);
+        }
+        sc.close();
+    }
+}
