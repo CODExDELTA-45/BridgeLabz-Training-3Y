253c23a9bbc931b7bb039e6daef367f2df9ae87d
Rohit Sharma <rohitsharma981818@gmail.com>
2025-09-03 16:26:57 +0530

    added

---
+++ Array-Practices/level1/FactorsArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class FactorsArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int maxFactor = 10;
+        int[] factors = new int[maxFactor];
+        int index = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                // Resize array if needed
+                if (index == factors.length) {
+                    int[] temp = new int[factors.length * 2];
+                    System.arraycopy(factors, 0, temp, 0, factors.length);
+                    factors = temp;
+                }
+                factors[index++] = i;
+            }
+        }
+
+        System.out.println("Factors of " + number + ":");
+        for (int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
+}
